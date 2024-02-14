e37731f820c8812887068ac034dd5cced6c586af
Adrienne Stilp <amstilp@uw.edu>
2024-02-14 15:30:12 -0800

    Show the user's vote on the detail page (if they voted)

--- django_htmx/polls/views.py
+++ django_htmx/polls/views.py
@@ -11,6 +11,16 @@ class QuestionDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["choices"] = self.object.choice_set.all()
+        vote = None
+        if self.request.user.is_authenticated:
+            try:
+                vote = models.Choice.objects.filter(
+                    question=self.object,
+                    vote__user=self.request.user
+                ).get()
+            except models.Choice.DoesNotExist:
+                pass
+        context["vote"] = vote
         return context
 
 
