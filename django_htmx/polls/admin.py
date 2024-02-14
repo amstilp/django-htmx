1dc6dac8050a259cb79fb124ef2e0ecd0fcd31b4
Adrienne Stilp <amstilp@uw.edu>
2024-02-14 12:36:10 -0800

    Register the polls models in the admin

--- django_htmx/polls/admin.py
+++ django_htmx/polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class ChoiceInline(admin.TabularInline):
+    model = models.Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ["text",]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ("text",)
+    search_fields = ("text",)
+
+
+admin.site.register(models.Question, QuestionAdmin)
+admin.site.register(models.Choice)
+admin.site.register(models.Vote)
