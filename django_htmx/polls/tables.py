3d511d51e33d798c2ea3bc07241788731a02b8e1
Adrienne Stilp <amstilp@uw.edu>
2024-02-14 15:34:02 -0800

    Show the number of votes on the question table

--- django_htmx/polls/tables.py
+++ django_htmx/polls/tables.py
@@ -6,6 +6,11 @@ from . import models
 class QuestionTable(tables.Table):
 
     pk = tables.Column(linkify=True, verbose_name="ID")
+    number_of_votes = tables.Column(empty_values=())
+
     class Meta:
         model = models.Question
         fields = ("pk", "text",)
+
+    def render_number_of_votes(self, record):
+        return models.Vote.objects.filter(choice__question=record).count()
