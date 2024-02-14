faaeabd3a48d342cc48d3be226864829a453e23a
Adrienne Stilp <amstilp@uw.edu>
2024-02-14 12:31:30 -0800

    Add basic models for polls app

--- django_htmx/polls/models.py
+++ django_htmx/polls/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.question.text}: {self.text}"
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} voted for {self.choice}'
